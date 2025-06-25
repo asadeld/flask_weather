f487c47ca7d3f63de8d99fd5f9c2d58920121bb6
Asad <asadeldarov06@gmail.com>
2025-06-25 16:58:26 +0300

    сделали главную страницу, добавили синхронизацию по api с сервисом openweathermap, вывели данные о погоде, сделали интерфейст страницы

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
 
-@app.route('/')
+API_KEY = '094d903b3c58e22de114cf2fa4d949f0'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    weather_data = None
+    error = None
+    if request.method == 'POST':
+        city = request.form['city']
+        p = {
+            'q': city,
+            'appid': API_KEY,
+            'units': 'metric',
+            'lang': 'ru'
+        }
+        response = requests.get(BASE_URL, params=p)
+        if response.status_code == 200:
+            data = response.json()
+            weather_data = {
+                'city': data['name'],
+                'temp': data['main']['temp'],
+                'feels_like': data['main']['feels_like'],
+                'description': data['weather'][0]['description'],
+                'humidity': data['main']['humidity'],
+                'icon': data['weather'][0]['icon']
+            }
+
+        else:
+            error = f'Город {city} не найден'
+
+    return render_template('index.html', weather=weather_data, error=error)
 
 @app.route('/contacts')
 def contacts():
